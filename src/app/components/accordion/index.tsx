c5d1fe8ef633ed994a8a911a652174743e44a368
Angie Rodriguez <angrodri902@gmail.com>
2024-03-12 15:36:08 -0700

    Initial commit

---
+++ src/app/components/accordion/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Component } from "react";
+import "./styles.css";
+
+class Accordion extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Accordion Class Component</h2>
+      </div>
+    );
+  }
+}
+
+export default Accordion;
